278afe7baf02c562e9de59e6a70bca4fd92b3341
CodyJones4235 <codyjones4325@gmail.com>
2021-09-16 23:14:11 -0500

    navbar

---
+++ newworldfrontend/src/components/nav/NavBar.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { Link, useHistory } from "react-router-dom"
+import "./NavBar.css"
+
+export const NavBar = () => {
+    const history = useHistory()
+
+    return (
+        <ul className="navbar">
+            <li className="navbar__item">
+                <Link to="/"></Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/Posts">View All Posts</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/CreatePost">Create New Post</Link>
+            </li>
+
+            {
+                (localStorage.getItem("newworld_token") !== null) ?
+                    <li className="navbar__item">
+                        <button className="navbar__link fakeLink"
+                            onClick={() => {
+                                localStorage.removeItem("newworld_token")
+                                history.push({ pathname: "/" })
+                            }}
+                        >Logout</button>
+                    </li> :
+                    <>
+                        <li className="navbar__item">
+                            <Link className="navbar__link" to="/login"> Login </Link>
+                        </li>
+                        <li className="navbar__item">
+                            <Link className="navbar__link" to="/register"> Register</Link>
+                        </li>
+                    </>
+            }        </ul>
+    )
+}
